9fbc57429ab4fd04497a8ed786da801cc6e7d65b
Mathius <mathiuskormasela12@gmail.com>
2023-06-24 02:57:20 +0700

    tipe data boolean

---
+++ src/TipeDataBoolean.java
@@ -0,0 +1,12 @@
+public class TipeDataBoolean {
+    public static void main(String[] args) {
+        // Tipe Data Boolean
+        // Tipe data yg hanya memiliki 2 nilai, yaitu true atau false
+        // Biasanya digunakan untuk operasi kondisional
+        boolean menikah = false;
+        boolean graduated = true;
+
+        System.out.println(menikah ? "Sudah Menikah" : "Belum Menikah");
+        System.out.println(graduated ? "Sudah Lulus" : "Belim Lulus");
+    }
+}
