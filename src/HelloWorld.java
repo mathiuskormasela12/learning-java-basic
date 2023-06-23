20a2f39fe6977ca311e174dedc1e5717191473be
Mathius <mathiuskormasela12@gmail.com>
2023-06-24 01:54:56 +0700

    hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,30 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        /*
+        * Dalam membuat Java File kita harus menuliskan
+        * nama class nya sama dengan nama file. Dan untuk nama class
+        * nya tidak boleh mengandung unsur spasi. Umumnya programmer
+        * Java akan menuliskna nama class nya itu di awali huruf besar
+        * untuk perkata. Misal HelloWorld, dimana huruf h da w akan di tulis
+        * menggunakan huruf kapital karena mereka adalah huruf pertama
+        * di masing-masing kata.
+        *
+        * Untuk menjalankan Java file, kita harus mengcompile file java
+        * menjadi binary file terlebih dahulu, yaitu dengan cara menuliskan
+        * di terminal keyword javac NamaFile.java lalu untuk menjalankan
+        * binary file nya kita bisa menulis java NamaFile.
+        *
+        * Step-Step
+        * 1. Compile Java file ke binary file
+        *    command:
+        *    javac NamaFile.java
+        * 2. Menjalankan binary file
+        *    command :
+        *    java NamaFile
+        * */
+        // Kode dibawah untuk menampilkan sesuatu kelayar. (Tidak boleh menggunakan single quote)
+        System.out.println("Hello World");
+    }
+
+}
